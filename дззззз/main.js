bb5603ab4ed3f9e59db2067046ddb9592ed76644
Nastya <125801230+AnastasiaMudrik@users.noreply.github.com>
2023-03-25 21:06:02 +0300

    Add files via upload

---
+++ дззззз/main.js
@@ -0,0 +1,5 @@
+var userData = ['Маша','Саша','Даша','Оля','Коля','Паша','Соня','Лена','Марина','Наташа'];
+var newData = userData.push('Аня','Катя','Женя','Вова');
+for(var index = 0; index<14; index++){
+    console.log(userData[index]+",приходи на др");
+}
